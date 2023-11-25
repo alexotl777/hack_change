ef9f0e79682f84e7aa376664dadde47971b27ad0
romanov9617 <romanov9617@gmail.com>
2023-11-25 16:58:33 +0300

    добавил модель заявки и какую=то валидацию к ней

--- routes/documents_package.py
+++ routes/documents_package.py
@@ -1,7 +1,9 @@
+import io, os, requests
+import zipfile
 from fastapi import APIRouter
 from fastapi.responses import FileResponse
 
-from hack_change.pydantic_models.document_package import PrimaryDocs
+from hack_change.pydantic_models.document_package import BidParticipantInfo
 from hack_change.functions.archiver import archivate_documents
 
 router = APIRouter(
@@ -12,8 +14,10 @@ router = APIRouter(
 # Предполагаю, что данные формы каким-то образом окажутся в хранилище. В базе данных лежат запрашиваемые условия.
 # Где-то в хранилище, на облаке или просто на сервере лежат паспортные данные. Для разработки файлы pdf будут лежать в папке 'тестовые файлы'
 
-@router.get("/dequeue_bids")
-async def get_dequeue_bids():
+
+
+@router.get('/docs_package')
+async def get_docs_package():
     """
     Рассматривать продукт будем один. Далее буду использовать продукт "Ипотечный кредит" - mortgage
     Возвращает список заявок из очереди для продукта c необходимым статусом
@@ -25,6 +29,7 @@ async def get_dequeue_bids():
     """
     archivate_documents()
 
-    return {
-        'documents': FileResponse('Документы пользователей/тестовые файлы.zip', filename='Документы пользователя.zip', media_type='application/zip'),
-    }
+    return FileResponse('Документы пользователей/тестовые файлы.zip', filename='Документы пользователя.zip', media_type='application/zip')
+
+async def get_bid_info():
+    pass
